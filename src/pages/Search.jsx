a105177496f7c33363fa34cca6ccff3a6ca71827
sumit verma <sumit.socialseller@gmail.com>
2024-03-16 22:51:23 +0530

    navigation added

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,19 +1,28 @@
 /* eslint-disable react/jsx-key */
 import { useSnapshot } from "valtio";
 import { state } from "../data/data";
-import {SuccessAlert} from "../components/Toast"
+import { SuccessAlert } from "../components/Toast";
 import Gallery from "../components/Gallery";
 import PageHeading from "../components/PageWrapper";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   const snap = useSnapshot(state);
   const title = snap.qeury;
   const images = snap.searchResult;
+  const navigate = useNavigate();
 
   const addToFavoriteHandler = (image) => {
     state.favorites.push(image);
-    SuccessAlert("Added to Favorites")
+    SuccessAlert("Added to Favorites");
   };
+
+  useEffect(() => {
+    if (images?.length === 0) {
+      navigate("/");
+    }
+  }, []);
 
   return (
     <PageHeading title={title}>
